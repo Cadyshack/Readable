7dd64a664df3bcbcea2c4d8e66c595358c925862
Christian Cadieux <cadieux_christian@hotmail.com>
2018-06-19 14:09:21 -0400

    feat: Built and styled PostListItems.js. More functionality style needed.

---
+++ src/pages/postList/PostListItem.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ArrowUp from 'react-icons/lib/md/keyboard-arrow-up';
+import ArrowDown from 'react-icons/lib/md/keyboard-arrow-down';
+import Comment from 'react-icons/lib/md/mode-comment';
+import './PostListItem.css';
+
+const PostListItem = (props) => {
+  const { title, author, category, commentCount, voteScore, timestamp, id, history } = props;
+  let date = new Date(timestamp);
+  const options = { year:'numeric', month:'long', day:'numeric', hour12: true, hour: '2-digit', minute: '2-digit' };
+  let formattedDate = date.toLocaleDateString("en-CA", options);
+
+	const link = (e) => {
+		history.push(`/${category}/${id}`);
+	}
+
+	const testing = (e) => {
+  	e.stopPropagation();
+  	console.log('Propagation stopped');
+  }
+  const upVote = (e) => {
+  	e.stopPropagation();
+  }
+  const downVote = (e) => {
+  	e.stopPropagation();
+  }
+
+  return (
+    <div className="post-list-item" onClick={link} >
+    	<div className="vote">
+    		<ArrowUp size={30} className="upVote" onClick={upVote} />
+    		<span>{voteScore}</span>
+    		<ArrowDown size={30} className="downVote" onClick={downVote} />
+    	</div>
+    	<div className="info">
+	    	<h2>{title}</h2>
+	    	<div className="author-info">
+	    		<span className="category-tag">{category}</span>
+	    		<span className="author">{`Posted by: ${author}`}</span>
+	    		<span className="date-info">{formattedDate}</span>
+
+	    	</div>
+	    	<div className="edit-delete">
+	    		<div>
+	    			<span><Comment size={30} className="comment" />{` ${commentCount} coments`}</span>
+	    		</div>
+	    		<div className="buttons">
+			    	<button className='ripple' onClick={testing}>Edit</button>
+			    	<button className='ripple' onClick={testing}>Delete</button>
+		    	</div>
+
+
+	    	</div>
+    	</div>
+
+    </div>
+  )
+}
+
+export default PostListItem;
